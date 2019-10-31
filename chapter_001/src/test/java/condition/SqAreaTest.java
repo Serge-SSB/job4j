26060d4b71c8e68190e9af81aeabccafb77914e4
Serge-SSB <serge.SSB.il@gmail.com>
2019-10-31 10:42:44 +0300

    перенос тестов в папку condition

---
+++ chapter_001/src/test/java/condition/SqAreaTest.java
@@ -0,0 +1,15 @@
+package condotion;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqAreaTest {
+    @Test
+    public void square() {
+        int in = 234;
+        int in2 = 23;
+        int expected = 207;
+        double out = SqArea.square(in, in2);
+        Assert.assertEquals(expected, out, 0.1f);
+    }
+}
