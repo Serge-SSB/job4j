1197e2b6466b30a9e513dc7476df9a08422210a9
Serge-SSB <serge.SSB.il@gmail.com>
2019-11-20 16:57:00 +0300

    5.6. Простые числа.[#187945]  Исправления

--- chapter_001/src/main/java/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/loop/CheckPrimeNumber.java
@@ -1,7 +1,7 @@
 package loop;
 
 public class CheckPrimeNumber {
-    public boolean check(int finish) {
+    public static boolean check(int finish) {
         boolean prime = (finish < 2) ? false : true;
         for (int i = 2; i < finish; i++) {
             if ((finish % i) == 0) {
--- chapter_001/src/main/java/loop/PrimeNumber.java
+++ chapter_001/src/main/java/loop/PrimeNumber.java
@@ -1,21 +1,15 @@
 package loop;
 
+import static loop.CheckPrimeNumber.check;
+
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        int i, j;
-        boolean prime;
-        for(i = 2; i <= finish; i++) {
-            prime = true;
-            // проверить, делится ли число без остатка
-            for (j = 2; j < i; j++)
-                // если число делится без остатка, значит, оно не простое
-                if((i % j) == 0) prime = false;
-            if (prime)
-                count++;
-
+        for(int a = 2; a <= finish; a++) {
+            boolean x = check(a);
+            if (x)
+            count++;
             }
-
         return count;
     }
 }
