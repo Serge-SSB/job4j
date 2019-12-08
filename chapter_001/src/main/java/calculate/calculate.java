bc60f318dbd9ccc86a967091a3a446a30e0cd939
Serge-SSB <serge.SSB.il@gmail.com>
2019-12-08 19:56:10 +0300

    5.8. JavaDoc. Code convention[#187942]

---
+++ chapter_001/src/main/java/calculate/calculate.java
@@ -0,0 +1,17 @@
+package calculate;
+
+/**
+ * Calculate
+ * @author Serge Barsanov (s.b.stal.invest@gmail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * main
+     * Hello,job4j
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Job4j.");
+    }
+}
---
+++ chapter_001/src/main/java/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ * 
+ * @author Serge Barsanov (s.b.stal.invest@gmail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package calculate;
