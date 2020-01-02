8df7c3e05044a5c911a944c2aef520deeb572627
Serge-SSB <serge.SSB.il@gmail.com>
2020-01-02 21:30:37 +0300

    6.4.1. Слово заканчивается с ...[#187961]

---
+++ chapter_001/src/main/java/array/EndsWith.java
@@ -0,0 +1,15 @@
+package array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = post.length - 1; i >= 0; --i) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+// проверить. что массив word имеет последние элементы одинаковые с post
