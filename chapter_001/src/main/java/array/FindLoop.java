f6d9f2876c78939a578c5dc610bac279213d6eed
Serge-SSB <serge.SSB.il@gmail.com>
2020-01-05 15:34:35 +0300

    6.5. Поиск индекса в диапазоне.[#187962]

--- chapter_001/src/main/java/array/FindLoop.java
+++ chapter_001/src/main/java/array/FindLoop.java
@@ -1,13 +1,24 @@
 package array;
 
 public class FindLoop {
-    public int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         int index;
         for (int i = 0; i < data.length; i++) {
             index = i;
             if (data[index] == el) {
                 rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (; start < finish; start++) {
+            if (data[start] == el) {
+                rst = start;
                 break;
             }
         }
